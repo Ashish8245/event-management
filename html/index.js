cc8b0457132e63c296c637e48eaa43ff8ea695bf
ashish8245 <ashish.kr8245@gmail.com>
2022-07-14 11:46:32 +0530

    newupdate

---
+++ html/index.js
@@ -0,0 +1,71 @@
+// // operator
+// // 1.arithmetics operator
+// let a = 5;
+// let b = 6;
+// //  console.log(a+b);
+// //  console.log(a-b);
+// //  console.log(a*b);
+// //  console.log(a/b);
+// // 2.assignment operator
+// let c= a;
+// c+=2;
+// console.log(c);
+// // 3.logical operator
+// // console.log(true && true);
+// // console.log(true && false);
+// // console.log(false && true);
+// // console.log(false && false);
+
+// // console.log(false || true);
+// // console.log(false || false);
+// // console.log(true || true);
+// // console.log(true || false);
+
+// let age=15;
+// switch(true){
+//     case(age>=18 && age<=55);
+//     document.write()
+
+// }
+
+
+
+
+
+
+
+// // methods of array
+// let myArr= ["ram","potato","108",null,true];
+// console.log(myArr.length)
+// myArr.pop()
+// console.log(myArr)
+
+// // console.log(myArr.length);
+// // myArr.pop();
+
+
+
+let myString= "ram is a good boy"
+console.log(myString.length);
+
+
+// let myDate = new Date();
+// // console.log(myDate)
+// // console.log(myDate.getTime());
+// console.log(myDate.getDay());
+// console.log(myDate.getHours());
+// console.log(myDate.getMonth());
+
+
+ 
+//  let elem= document.getElementById('myBtn');
+//  console.log(elem);
+//  let elemClass= document.getElementsByClassName("container")
+//  console.log
+ 
+ let elemClass = document
+ getElementsByClassName("container")
+ console.log(elemClass);
+
+ elemClass[0].style.background= "yellow";
+
